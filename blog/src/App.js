16988faacf8da829b3c9016fa77769265bc462f9
Daniel <byoung.mokk@gmail.com>
2022-12-17 20:24:10 +0900

    변수&스타일 적용 Update

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
+/*
+  리액트에서 <div> 만드는법 :
+    React.createElement('div', null, 'Hello Would');
+
+  JSX를 이용한 방법 : 리액트안에서 html문법으로 작성하면 리액트로 받아들인다.
+    <div></div>
+*/
+
 function App() {
+  let post = ['자바 독학', '리액트 독학', '자바 스크랩트 독학'];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="blog">
+        <h2>Blog</h2>
+      </div>
+
+      <div >
+        {/* 블로그 글 제목들 */}
+        <h4 className="post">{post[0]}</h4>
+        <h4 className="post">{post[1]}</h4>
+        {/* 스타일을 넣는 다른 방법 */}
+        <h4 style=
+        {
+          {
+            paddingLeft : '20px',
+            textAlign : 'left',
+            borderBottom : '1px solid grey'
+          }
+        } >{post[2]}</h4>
+      </div>
+
     </div>
   );
 }
