3090241fce360f194231aea8657d3e1e17b7dc3f
Daniel <byoung.mokk@gmail.com>
2022-12-25 14:25:45 +0900

    feat: Add react-router-dom
    router 기능을 이용한 화면전환

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,9 +1,19 @@
 import Home from "./pages/Home";
+import About from "./pages/About";
+import React from "react";
+import {Routes, Route, Link} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+        <nav>
+            {/* Link : <a>와 같은 기능을 한다.*/}
+            <Link to="/">Home</Link> | <Link to="/about">About</Link>
+        </nav>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+        </Routes>
     </div>
   );
 }
