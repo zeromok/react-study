c6bdd02cfcc8576c1596904c6608b29bf2885f57
Daniel <byoung.mokk@gmail.com>
2022-12-25 14:43:32 +0900

    Feat: "카운터 기능 구현"
     useState를 활용한 버튼식 카운터 기능을 넣어봄

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,5 +1,6 @@
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Counter from "./pages/Counter";
 import React from "react";
 import {Routes, Route, Link} from "react-router-dom";
 
@@ -8,11 +9,12 @@ function App() {
     <div className="App">
         <nav>
             {/* Link : <a>와 같은 기능을 한다.*/}
-            <Link to="/">Home</Link> | <Link to="/about">About</Link>
+            <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/counter">Counter</Link>
         </nav>
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/counter" element={<Counter />} />
         </Routes>
     </div>
   );
