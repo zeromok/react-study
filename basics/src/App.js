3b811e43500acf4d9856fa83fb5abe000bb9524e
zeromok <byoung.mokk@gmail.com>
2023-01-05 22:41:09 +0900

    Feat: Add 사용자가 입력한 멀티값 가져오는 기능구현

--- basics/src/App.js
+++ basics/src/App.js
@@ -2,6 +2,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Counter from "./pages/Counter";
 import Input from "./pages/Input";
+import Input2 from "./pages/Input2";
 import React from "react";
 import {Routes, Route, Link} from "react-router-dom";
 
@@ -10,13 +11,14 @@ function App() {
     <div className="App">
         <nav>
             {/* Link : <a>와 같은 기능을 한다.*/}
-            <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/counter">Counter</Link> | <Link to="/input">Input</Link>
+            <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/counter">Counter</Link> | <Link to="/input">Input</Link> | <Link to="/input2">Input2</Link>
         </nav>
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/counter" element={<Counter />} />
             <Route path="/Input" element={<Input />} />
+            <Route path="/Input2" element={<Input2 />} />
         </Routes>
     </div>
   );
